7b89b47cea49862d9a99a467211952ed4ca815d8
Hyeongseok Kim <neoself1105@gmail.com>
2025-06-04 16:39:46 +0900

    refactor: HomeView 수정 (#3)
    - CampaignSection -> CampainBanner로 명칭 수정

---
+++ cherrydan/Presentation/Home/CampaignBanner.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+
+struct CampaignBanner: View {
+    let campaigns: [[String]]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(campaigns, id: \.self) { campaign in
+                    campainBannerItem(campaign)
+                        .padding(16)
+                        .frame(width: UIScreen.main.bounds.width)
+                        .scrollTransition { content, phase in
+                            content
+                                .opacity(phase.isIdentity ? 1.0 : 0.8)
+                                .scaleEffect(phase.isIdentity ? 1.0 : 0.95)
+                        }
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.viewAligned)
+    }
+    
+    private func campainBannerItem(_ campaign:[String])-> some View {
+        ZStack {
+            Text("\(campaign[2])/3")
+                .font(.m6r)
+                .foregroundStyle(.gray1)
+                .padding(.horizontal,12)
+                .frame(height: 24)
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                    .fill(.mPink3)
+                )
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(campaign[0])
+                    .font(.t4)
+                    .foregroundColor(.gray1)
+                
+                Text(campaign[1])
+                    .font(.t5)
+                    .foregroundColor(.gray1)
+            }
+            .frame(maxWidth: .infinity, maxHeight: 80, alignment: .leading)
+        }
+        .padding(12)
+        .background(
+            RoundedRectangle(cornerRadius: 4)
+                .fill(.mPink3)
+        )
+    }
+}
--- cherrydan/Presentation/Home/HomeView.swift
+++ cherrydan/Presentation/Home/HomeView.swift
@@ -2,32 +2,39 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var selectedTab = 0
+    
     let buttonArr = ["인기 공고", "마감임박", "새로운 공고", "선정확률 높은", "관심 공고", "지역 맞춤"]
     
+    let campaigns = [
+        ["체험단 캠페인 여기 다 모았다!","지금 최고의 체험단에 신청해 보세요.", "1"],
+        ["체험단 캠페인 여기 다 모았다!","지금 최고의 체험단에 신청해 보세요.", "2"],
+        ["체험단 캠페인 여기 다 모았다!","지금 최고의 체험단에 신청해 보세요.", "3"]
+    ]
+    
     var body: some View {
-        ScrollView {
-            VStack(spacing: 20) {
-                header
-                    .padding(.horizontal, 16)
-                
-                CampaignSection()
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 0) {
-                        ForEach(0..<buttonArr.count, id: \.self) { index in
-                            categorySection(index)
-                                .onTapGesture { selectedTab = index }
-                        }
-                        .padding(.horizontal, 8)
-                    }
-                    .padding(.horizontal, 8)
+        VStack(spacing: 0) {
+            CDHeaderWithLeftContent() {
+                HStack(alignment: .center) {
+                    Text("카테고리")
+                        .font(.t1)
                     
+                    Image("hamburger")
                 }
-                
-                campaignGridSection
+            }
+            
+            ScrollView {
+                VStack(spacing: 20) {
+                    CampaignBanner(campaigns: campaigns)
+                    
+                    CDTabSection(
+                        selectedTab: $selectedTab,
+                        data: buttonArr
+                    )
+                    
+                    campaignGridSection
+                }
             }
         }
-        .animation(.spring, value: selectedTab)
     }
     
     private var campaignGridSection: some View {
@@ -50,44 +57,8 @@ struct HomeView: View {
         }
         .padding(.horizontal)
     }
-    
-    private func categorySection(_ index:Int) -> some View {
-        VStack(spacing: 8){
-            Text(buttonArr[index])
-                .font(.m3b)
-                .foregroundColor(selectedTab == index ? .mPink3 : .gray4)
-                .padding(.horizontal, 4)
-            
-            Rectangle()
-                .fill(index == selectedTab ? .mPink3 : .clear)
-                .frame(height: 2)
-        }
-    }
 }
 
-private var header: some View {
-    HStack(alignment: .center) {
-        Text("카테고리")
-            .font(.t1)
-        Image("hamburger")
-        
-        Spacer()
-        
-        HStack(spacing: 4) {
-            Button(action: {}) {
-                Image("alarm")
-            }
-            
-            Button(action: {}) {
-                Image("search")
-            }
-        }
-    }
-    .padding(.top, 8)
+#Preview {
+    HomeView()
 }
-
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
-} 
