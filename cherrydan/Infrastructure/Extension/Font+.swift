34b20706e742de8254f56caabe0edc02c9e67617
Hyeongseok Kim <neoself1105@gmail.com>
2025-08-08 17:14:52 +0900

    refactor: 전체 폰트 크기 일괄 조정 (#11)

--- cherrydan/Infrastructure/Extension/Font+.swift
+++ cherrydan/Infrastructure/Extension/Font+.swift
@@ -3,24 +3,24 @@ import SwiftUI
 
 extension Font {
     // Title Text - GangwonEduAll OTF
-    static let t1 = Font.custom("GangwonEduAll-OTFLight", size: 28)
-    static let t2 = Font.custom("GangwonEduAll-OTFBold", size: 24)
-    static let t3 = Font.custom("GangwonEduAll-OTFLight", size: 20)
-    static let t4 = Font.custom("GangwonEduAll-OTFBold", size: 18)
-    static let t5 = Font.custom("GangwonEduAll-OTFLight", size: 12)
+    static let t1 = Font.custom("GangwonEduAll-OTFLight", size: 30)
+    static let t2 = Font.custom("GangwonEduAll-OTFBold", size: 26)
+    static let t3 = Font.custom("GangwonEduAll-OTFLight", size: 22)
+    static let t4 = Font.custom("GangwonEduAll-OTFBold", size: 20)
+    static let t5 = Font.custom("GangwonEduAll-OTFLight", size: 14)
     
     // Main Text Bold - Happiness Sans
-    static let m1b = Font.custom("Happiness-Sans-Bold", size: 20)
-    static let m2b = Font.custom("Happiness-Sans-Bold", size: 18)
-    static let m3b = Font.custom("Happiness-Sans-Bold", size: 16)
-    static let m4b = Font.custom("Happiness-Sans-Bold", size: 14)
-    static let m5b = Font.custom("Happiness-Sans-Bold", size: 12)
+    static let m1b = Font.custom("Happiness-Sans-Bold", size: 22)
+    static let m2b = Font.custom("Happiness-Sans-Bold", size: 20)
+    static let m3b = Font.custom("Happiness-Sans-Bold", size: 18)
+    static let m4b = Font.custom("Happiness-Sans-Bold", size: 16)
+    static let m5b = Font.custom("Happiness-Sans-Bold", size: 14)
     
     // Main Text Regular - Happiness Sans
-    static let m1r = Font.custom("Happiness-Sans-Regular", size: 20)
-    static let m2r = Font.custom("Happiness-Sans-Regular", size: 18)
-    static let m3r = Font.custom("Happiness-Sans-Regular", size: 16)
-    static let m4r = Font.custom("Happiness-Sans-Regular", size: 14)
-    static let m5r = Font.custom("Happiness-Sans-Regular", size: 12)
-    static let m6r = Font.custom("Happiness-Sans-Regular", size: 10)
+    static let m1r = Font.custom("Happiness-Sans-Regular", size: 22)
+    static let m2r = Font.custom("Happiness-Sans-Regular", size: 20)
+    static let m3r = Font.custom("Happiness-Sans-Regular", size: 18)
+    static let m4r = Font.custom("Happiness-Sans-Regular", size: 16)
+    static let m5r = Font.custom("Happiness-Sans-Regular", size: 14)
+    static let m6r = Font.custom("Happiness-Sans-Regular", size: 12)
 }
